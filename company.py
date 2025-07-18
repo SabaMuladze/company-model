79430f2b18463cfacfcbfc3693b5938f8bae076a
SabaMuladze <muladze66@bk.ru>
2025-07-18 20:07:15 +0400

    add Developer class as a child of Employee

--- company.py
+++ company.py
@@ -12,11 +12,23 @@ class Employee:
      self.salary = self.salary * self.increase_percent
      return self.salary
 
-employee1 = Employee('Saba','Muladze',1000)
+class Developer(Employee):
+   increase_percent = 1.2
+   def __init__(self, first, last, salary,language):
+      super().__init__(first, last, salary)
+      self.language = language
+    
+
+# test Employee part
+employee1 = Employee('Saba','Muladze',5000)
 print(employee1)
 employee1.increase_salary()
 print(employee1.salary)
-        
+#test Developer part
+developer1 = Developer('Givi','Pailodze',6000,'Python')
+print(developer1)
+developer1.increase_salary()
+print(developer1.salary)
 
 
         
