b876a654a74941599d5f0e5dde5fe309a9b2b471
SabaMuladze <muladze66@bk.ru>
2025-07-18 20:29:57 +0400

    add Manager class as child of Employee

--- company.py
+++ company.py
@@ -13,10 +13,35 @@ class Employee:
      return self.salary
 
 class Developer(Employee):
-   increase_percent = 1.2
-   def __init__(self, first, last, salary,language):
+    increase_percent = 1.2
+    def __init__(self, first, last, salary,language):
       super().__init__(first, last, salary)
       self.language = language
+          
+
+class Manager(Employee):
+    increase_percent = 1.1
+
+    def __init__(self, first, last, salary,team):
+      super().__init__(first, last, salary)
+      self.team = team
+    
+    def add_in_team(self,employee):
+      self.team.append(employee)
+    
+    def delete_from_team(self,employee):
+       self.team.remove(employee)
+    
+    def show_team(self):
+        if len(self.team) == 0:
+          print('Team is empty')
+        else:
+            print(f"###### {self.first} {self.last}'s team is:")
+            for member in self.team:  
+                print(member)
+            print('######')
+          
+      
     
 
 # test Employee part
@@ -26,9 +51,21 @@ employee1.increase_salary()
 print(employee1.salary)
 #test Developer part
 developer1 = Developer('Givi','Pailodze',6000,'Python')
+developer2 = Developer('Mariam','Nozadze',6100,'JS')
 print(developer1)
 developer1.increase_salary()
 print(developer1.salary)
+# test Manager part
+manager1 = Manager('Sofo','Menejerishvili',5500,[developer1])
+print(manager1)
+manager1.increase_salary()
+print(manager1.salary)
+manager1.show_team()
+manager1.add_in_team(employee1)
+manager1.add_in_team(developer2)
+manager1.show_team()
+manager1.delete_from_team(employee1)
+manager1.show_team()
 
 
         
